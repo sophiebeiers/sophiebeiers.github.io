9f62bb4c983c9713e7c95e0b1025662c02e611ad
Sophie Beiers <sbeiers@aclu.org>
2019-05-21 23:29:26 -0400

    startin on license plates

---
+++ license-plates-master/src/scrolly.js
@@ -0,0 +1,32 @@
+import enterView from 'enter-view'
+import * as d3 from 'd3'
+
+// If this module changes, refresh the ENTIRE page
+if (module.hot) {
+  module.hot.accept(() => window.location.reload())
+}
+
+// Scroll actions for dots
+enterView(
+  {
+    selector: '.step',
+    offset: 0.9,
+    enter: function(element) {
+      element.classList.add('entered')
+      // Trigger stepin for current step
+      d3.select(element).dispatch('stepin')
+    },
+    exit: function(element) {
+      element.classList.remove('entered')
+
+      // Trigger stepout for current step
+      d3.select(element).dispatch('stepout')
+
+      // Trigger stepin for previous step (if it exists)
+      var previous = element.previousElementSibling
+      if (previous && previous.classList.contains('step')) {
+        d3.select(previous).dispatch('stepin')
+      }
+    }
+  }
+)
