04adec5f3fb4c156dadf4c1e8eefcf0c340f0527
Sophie Beiers <sbeiers@aclu.org>
2019-06-09 18:15:17 -0400

    gettin' there

--- proj_bachelor/season15/bubbles.js
+++ proj_bachelor/season15/bubbles.js
@@ -1,96 +1,134 @@
 (function() {
   var width = 900;
-  height = 650;
+  height = 750;
 
-let margin = { top: 0, left: 0, right: 0, bottom: 0 }
-let svg = d3
-  .select('#chart')
-  .append('svg')
-  .attr('height', height + margin.top + margin.bottom)
-  .attr('width', width + margin.left + margin.right)
-  .append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+  let margin = {
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  }
+  let svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('height', height + margin.top + margin.bottom)
+    .attr('width', width + margin.left + margin.right)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
-// defs for photos
-var defs = svg.append("defs");
+  // defs for photos
+  var defs = svg.append("defs");
 
-// data
-Promise.all([
-  d3.csv('bach.csv'),
-])
-  .then(ready)
-  .catch(err => {
-    console.log('Failed with', err)
-  })
-
-
-function ready([
-  data
-]) {
-
-var grouped_boys = d3.nest()
-  .key(function(d) { return d.id;})
-  .entries(data);
-
-var xScale = d3
-.scaleLinear()
-.range([90, width- 100])
-.domain([1, 8])
-
-var weeks = ["Week1", "Week 2", "Week3", "Week4", "Week5", "Week6", "Week7", "Week8"]
-svg.append("g")
-  .attr("transform", "translate(0," + 20 + ")")
-  .call(d3.axisTop(xScale).ticks(8).tickFormat(function(d,i){return weeks[i]}))
-  .selectAll("text")
-    .attr("class", "x-axis-text")
-    .style("text-anchor", "middle");
+  // data
+  Promise.all([
+      d3.csv('bach.csv'),
+    ])
+    .then(ready)
+    .catch(err => {
+      console.log('Failed with', err)
+    })
 
 
-var yScale = d3
-.scaleLinear()
-.range([10, height])
-.domain([0, 45])
+  function ready([
+    data
+  ]) {
 
-// the connecting lines
-svg.selectAll(".line")
+
+    var grouped_boys = d3.nest()
+      .key(function(d) {
+        return d.id;
+      })
+      .entries(data);
+
+
+    var xScale = d3
+      .scaleLinear()
+      .range([90, width - 100])
+      .domain([1, 8])
+
+    var weeks = ["Week1", "Week 2", "Week3", "Week4", "Week5", "Week6", "Week7", "Week8"]
+    svg.append("g")
+      .attr("transform", "translate(0," + 20 + ")")
+
+      .call(d3.axisTop(xScale).ticks(8).tickFormat(function(d, i) {
+        return weeks[i]
+      }))
+      .selectAll("text")
+      .attr("class", "x-axis-text")
+      .style("text-anchor", "middle");
+
+
+    var yScale = d3
+      .scaleLinear()
+      .range([10, height])
+      .domain([0, 40])
+    //
+
+    // axis labels
+    svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0)
+      .attr("x", 0 - (height - 300))
+      .attr("dy", "1em")
+      .attr("class", "x-axis-text")
+      .style("text-anchor", "middle")
+      .html("Picked early   " + "&#x2192;");
+
+    svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - 0)
+      .attr("x", 0 - (height - 50))
+      .attr("dy", "1em")
+      .attr("class", "x-axis-text")
+      .style("text-anchor", "middle")
+      .html("&#x2190;" + "    Picked late  ");
+
+
+console.log('hey')
+    // the connecting lines
+    svg.selectAll(".line")
       .data(grouped_boys)
       .enter()
       .append("path")
-        .attr("fill", "none")
+      .attr("fill", "none")
       .attr('class', d => {
         return d['key']
       })
       .attr("stroke", "pink")
-        .attr("stroke-width", 0.7)
-        .attr("d", function(d){
-          return d3.line()
-            .x(function(d) { return xScale(d.week); })
-            .y(function(d) { return yScale(+d.place) * 1.9; })
-            (d.values)
-        })
+      .attr("stroke-width", 0.7)
+      .attr("d", function(d) {
+        return d3.line()
+          .x(function(d) {
+            return xScale(d.week);
+          })
+          .y(function(d) {
+            return yScale(+d.place) * 1.9;
+          })
+          (d.values)
+      })
 
-// the images
-  defs.selectAll(".men-pattern")
-    .data(data)
-    .enter().append("pattern")
-    .attr("class", "men-pattern")
-    .attr("id", function(d){
-      return d.id
-    })
-    .attr("height", "100%")
-    .attr("width", "100%")
-    .attr("patternContentUnits", "objectBoundingBox")
-    .append("image")
-    .attr("height", 1)
-    .attr("width", 1)
-    .attr("preserveAspectRatio", "none")
-    .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
-    .attr("xlink:href", function(d){
-      return d.image_path
-    });
+    // the images
+    defs.selectAll(".men-pattern")
+      .data(data)
+      .enter().append("pattern")
+      .attr("class", "men-pattern")
+      .attr("id", function(d) {
+        return d.id
+      })
+      .attr("height", "100%")
+      .attr("width", "100%")
+      .attr("patternContentUnits", "objectBoundingBox")
+      .append("image")
+      .attr("height", 1)
+      .attr("width", 1)
+      .attr("preserveAspectRatio", "none")
+      .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
+      .attr("xlink:href", function(d) {
+        return d.image_path
+      });
 
-// circles
-  svg.selectAll(".men")
+    // circles
+    svg.selectAll(".men")
       .data(data)
       .enter()
       .append('circle')
@@ -103,16 +141,20 @@ svg.selectAll(".line")
       .attr("id", "men")
 
       .attr("r", 15)
-      .attr("cx", function(d){return xScale(d.week)})
-      .attr("cy", function(d){return yScale(d.place) * 1.9})
-      .attr("fill", function(d){
+      .attr("cx", function(d) {
+        return xScale(d.week)
+      })
+      .attr("cy", function(d) {
+        return yScale(d.place) * 1.9
+      })
+      .attr("fill", function(d) {
         return "url(#" + d.id + ")"
       })
-      // .attr("data-tippy-content", d => d.data.week);
+    // .attr("data-tippy-content", d => d.data.week);
 
-// mouseover events
+    // mouseover events
     svg.selectAll('circle')
-      .on('mouseover', function (d) {
+      .on('mouseover', function(d) {
         var className = d['id']
         var circleUnderMouse = this;
         console.log(this)
@@ -122,21 +164,21 @@ svg.selectAll(".line")
           .attr('stroke', 'red')
           // bring to front
           .each(function() {
-          this.parentNode.appendChild(this);
+            this.parentNode.appendChild(this);
           });
         d3.selectAll('circle.' + className)
           .raise()
           .attr('r', 30)
           .attr("stroke", "pink")
           .attr('stroke-width', 2)
-          // make all the other ones fade
+        // make all the other ones fade
         d3.selectAll('circle')
           .transition()
-          .style('opacity', function () {
+          .style('opacity', function() {
             return (this.getAttribute('class') === className) ? 1 : 0.5;
-  })
-})
-      .on('mouseout', function (d) {
+          })
+      })
+      .on('mouseout', function(d) {
         var className = d['id']
         d3.selectAll('path.' + className)
           .transition()
@@ -153,33 +195,85 @@ svg.selectAll(".line")
 
       })
 
-tippy('svg circle', {
-  arrow: true,
-  size: 'large',
-  animation: 'scale',
-  theme: 'pink',
-})
-
-
-// steps
-d3.select('#two').on('stepin', () => {
-  console.log("hey")
-
-    svg
-      .selectAll('circle')
-      .transition()
-      .duration(800)
-      .attr('r', function(d){
-        if(d.week == "1"){
-          return 20
-        }
-        else{
-          return 15
-        }
-      })
-
-
+    tippy('svg circle', {
+      arrow: true,
+      size: 'large',
+      animation: 'scale',
+      theme: 'pink',
     })
+
+
+    // steps
+    d3.select('#two').on('stepin', () => {
+      svg
+        .selectAll('circle')
+        .attr('opacity', function(d) {
+          if (d.week == "1") {
+            return 1
+          } else {
+            return 0.2
+          }
+        })
+        .transition()
+        .duration(800)
+
+        .attr('fill', function(d) {
+          if (d.left == "yes" && d.week == "1") {
+            return "black"
+          } else {
+            return "url(#" + d.id + ")"
+          }
+        })
+    })
+
+    d3.select('#three').on('stepin', () => {
+      svg
+        .selectAll('circle')
+        .transition()
+        .duration(800)
+        .attr('opacity', function(d) {
+          if (d.week == "2") {
+            return 1
+          } else {
+            return 0.2
+          }
+        })
+        .transition()
+        .duration(800)
+
+        .attr('fill', function(d) {
+          if (d.left == "yes" && d.week == "2") {
+            return "black"
+          } else {
+            return "url(#" + d.id + ")"
+          }
+        })
+    })
+
+    d3.select('#four').on('stepin', () => {
+      svg
+        .selectAll('circle')
+        .transition()
+        .duration(800)
+        .attr('opacity', function(d) {
+          if (d.week == "3") {
+            return 1
+          } else {
+            return 0.2
+          }
+        })
+        .transition()
+        .duration(800)
+
+        .attr('fill', function(d) {
+          if (d.left == "yes" && d.week == "3") {
+            return "black"
+          } else {
+            return "url(#" + d.id + ")"
+          }
+        })
+    })
+
 
   } // end of data section
 
